98397040589d1245e60fb30cee80f5570e39a76a
Jose Candia <josecandiap@gmail.com>
2023-02-19 17:42:43 -0300

    fix: se elimina metodo sin utilizar

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,11 +5,6 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('users')
-  getUsers(): string[] {
-    return ['Ironman', 'Spiderman', 'Captain America'];
-  }
-
   @Get()
   getHello(): string {
     return this.appService.getHello();
