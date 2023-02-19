92484a34a0c143536db3476a075ec2a0ee65079e
Jose Candia <josecandiap@gmail.com>
2023-02-19 17:43:46 -0300

    feat: se agrega inyeccion de model

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,19 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { Product, ProductDocument } from './schema/products.model';
 
 @Injectable()
 export class ProductsService {
+
+  constructor(@InjectModel(Product.name) private productsModel: Model<ProductDocument>) {}
+
   create(createProductDto: CreateProductDto) {
     return 'This action adds a new product';
   }
 
-  findAll() {
-    return `This action returns all products`;
+  async findAll(): Promise<Product[]> {
+    return this.productsModel.find().exec();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  findOne(id: string): Promise<Product> {
+    return this.productsModel.findOne({ _id: id }).exec();
   }
 
   update(id: number, updateProductDto: UpdateProductDto) {
