82a1eae8ab644402a9972864fe936eb939dd186b
Jose Candia <josecandiap@gmail.com>
2023-02-19 17:43:02 -0300

    feat: se importa modulo conexion mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 @Module({
-  imports: [ProductsModule],
+  imports: [
+    ProductsModule,
+    MongooseModule.forRoot(process.env.MONGO_URI, {
+      dbName: 'desafio-08'
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
